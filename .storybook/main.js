e80e33cbd6bf9b8999b427c48fb0ad4d6a66c132
Lakshmanan A <uilaxman@gmail.com>
2022-01-23 00:19:46 +0530

    Rebranded the design

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,11 +9,12 @@ const postcssPlugins = require('../config/postcss-plugins');
 const enableDocs = !parseInt(process.env.STORYBOOK_DISABLE_DOCS || '0', 10);
 
 module.exports = {
-  stories: ['../playground/stories.tsx', '../src/components/**/*/README.md'],
+  stories: ['../src/components/**/*/README.md'],
   addons: [
     {name: '@storybook/addon-essentials', options: {docs: enableDocs}},
     '@storybook/addon-a11y',
     '@storybook/addon-contexts',
+    '@storybook/addon-docs'
   ],
   webpackFinal: (config) => {
     const isProduction = config.mode === 'production';
